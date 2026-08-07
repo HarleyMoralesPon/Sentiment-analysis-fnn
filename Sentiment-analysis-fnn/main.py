e3bae05a884b947fd24b81e30bac57186f57af84
HarleyMoralesPon <hmoralespon@gmail.com>
2026-08-07 15:31:55 +0200

    Implement text preprocessing pipeline

--- Sentiment-analysis-fnn/main.py
+++ Sentiment-analysis-fnn/main.py
@@ -1,7 +1,8 @@
 from src.preprocessing import load_dataset
+from src.preprocessing import clean_text
+from src.preprocessing import preprocess_dataframe
 
 df = load_dataset ("C:\\Users\\HarleyRicardoMorales\\OneDrive - BBQ - Baumann Bildung und Qualifizierung GmbH\\Dokumente\\ML-Projects\\Sentiment-analysis-fnn\\data\\raw\\sentimentdataset.csv")
+
+df = preprocess_dataframe(df)
 print (df.head ())
-
-
-
--- Sentiment-analysis-fnn/src/preprocessing.py
+++ Sentiment-analysis-fnn/src/preprocessing.py
@@ -1,6 +1,9 @@
 import re
 import pandas as pd 
 
+'''
+Function 1: load dataset
+'''
 def load_dataset(filepath):
     """
     Load the sentiment dataset.
@@ -19,3 +22,38 @@ def load_dataset(filepath):
     df = pd.read_csv(filepath)
 
     return df
+
+
+'''
+Function 2: clean text
+
+'''
+def clean_text (text):
+    text = text.lower() # convert to lowercase
+
+    text = re.sub(r"http\S+|www\S+", "", text) # remove URLS
+
+    text = re.sub(r"@\w+", "", text) # remove mentions
+
+    text = re.sub(r"[^\w\s]", "", text) # remove punctuation
+
+    text = re.sub(r"\d+", "", text) # remove numbers
+
+    text = re.sub(r"\s+", " ", text).strip() # remove extra whitespace
+
+    return text
+
+
+
+'''
+Function 3: preprocess dataset
+'''
+def preprocess_dataframe(df):
+    df = df.copy ()
+    df["Text"] = df["Text"].apply(clean_text)
+    return df
+
+'''
+- Now apply the cleaning function.
+- For example, if your tweet column is called "Text":
+'''
